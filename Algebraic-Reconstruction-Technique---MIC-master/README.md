86b8b259b7a427dd1d8594f0f480c90a8d809d61
Ritul Shekhar <136343118+ritulshekhar@users.noreply.github.com>
2025-04-02 16:11:17 +0530

    Add files via upload

---
+++ Algebraic-Reconstruction-Technique---MIC-master/README.md
@@ -0,0 +1,6 @@
+# Algebraic-Reconstruction-Technique---MIC
+
+This project is a MATLAB implementation of the variants of ART, a popular CT Image reconstruction problem. 
+
+We explore the following 3 variants of ART (Additive, Multiplicative and Simultaneous) and perform comparisons in the results of the reconstructed image compared with the original image, whose Radon Transform we had acquired. We have experimented on 2 test images, the phantom image and a Brain MRI image. The reported values are the RRMSE i.e. the relative Root mean squared value. We also have results for the various different parameters available in the algorithm.
+
---
+++ Algebraic-Reconstruction-Technique---MIC-master/code/multiplicativeART.m
@@ -0,0 +1,52 @@
+function [attenuation, rrmse_list] = multiplicativeART(radon_transform, imaging_matrix, n_iter, num_views, start_ang, del_ang, stop_ang, lambda, original_image)
+	% Constructs the Attenuation Matrix %
+	fprintf('Multiplicative Algorithm \n');
+	
+	[h,w] = size(squeeze(imaging_matrix(1,:,:)));
+	attenuation = ones([h,w]);
+
+	rrmse_list = zeros(size([n_iter,1]));
+
+	% % GIF part
+	% imagesc(attenuation)
+	% fCount = 60;
+	% detlaT = 0.1;
+	% f = getframe(gcf);
+	% [im,map] = rgb2ind(f.cdata,256,'nodither');
+	% k = 1;
+
+	for i=1:n_iter
+		fprintf('Iteration %d \n', i);
+		for curr_view = 1:num_views
+			curr_ang = start_ang + (curr_view - 1)*del_ang;
+			curr_matrix = squeeze(imaging_matrix(curr_view,:,:));
+			% size(curr_matrix)
+			rotated_attenuation = imrotate(attenuation, curr_ang, 'bilinear', 'crop');
+			batch_back_proj = sum(rotated_attenuation.*curr_matrix, 1);
+			denominator = squeeze(batch_back_proj(batch_back_proj > 0));
+			radon_term  = squeeze(radon_transform(curr_view, :));
+			update_term = ones(size(batch_back_proj));
+			update_term(batch_back_proj > 0) = radon_term(batch_back_proj > 0)./denominator;
+			% size(update_term)
+			update_vector = repmat(update_term,[h,1]).*curr_matrix;
+			% size(update_vector)
+			attenuation = attenuation.*(imrotate(update_vector,-curr_ang,'bilinear','crop').^lambda);
+
+			% Non negativity constraint as projection onto convex set
+			attenuation(attenuation<0) = 0;
+			attenuation(attenuation>1) = 1;
+
+			% % GIF Part
+			% if (mod(curr_view, 30) == 0)
+			% 	imagesc(attenuation);
+  	% 			f = getframe(gcf);
+  	% 			im(:,:,1,k) = rgb2ind(f.cdata,map,'nodither');
+  	% 			k = k + 1;
+  	% 		end
+% 			imagesc(attenuation);
+% 			waitforbuttonpress;
+		end
+		rrmse_list(i) = RRMSE(original_image, attenuation);
+	end
+	% imwrite(im,map,'Animation-Multiplicative.gif','DelayTime',detlaT,'LoopCount',inf)
+end
